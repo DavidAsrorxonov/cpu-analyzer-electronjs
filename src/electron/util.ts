2252f1796e8d456d38b9e73e3a70c6d592c6c58e
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-09-08 15:28:31 +0900

    feat: refactor IPC handling by introducing ipcHandle function for better abstraction and type-safety

--- src/electron/util.ts
+++ src/electron/util.ts
@@ -1,3 +1,12 @@
+import { ipcMain } from "electron";
+
 export function isDev(): boolean {
   return process.env.NODE_ENV === "development";
+}
+
+export function ipcHandle<Key extends keyof EventPayloadMapping>(
+  key: string,
+  handler: () => EventPayloadMapping[Key]
+) {
+  ipcMain.handle(key, () => handler());
 }
--- types.d.ts
+++ types.d.ts
@@ -11,6 +11,11 @@ type StaticData = {
   totalMemory: number;
 };
 
+type EventPayloadMapping = {
+  statistics: Statistics;
+  getStaticData: StaticData;
+};
+
 interface Window {
   electron: {
     subscribeStatistics: (callback: (statistics: Statistics) => void) => void;
